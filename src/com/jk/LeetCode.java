10ef579e8d0baa6c4b0f26d54f0d767eeb19a400
JackyQu <junjie.qu@symbio.com>
2017-09-21 17:23:30 +0800

    first commit

---
+++ src/com/jk/LeetCode.java
@@ -0,0 +1,16 @@
+package com.jk;
+
+import com.jk.solution.ReverseInteger_7;
+
+public class LeetCode {
+
+    public static void main(String[] args) {
+        System.out.println("JK");
+        test7();
+    }
+
+    private static void test7() {
+        System.out.println("7 ReverseInteger:");
+        System.out.println(ReverseInteger_7.reverse(153426));
+    }
+}
---
+++ src/com/jk/solution/ReverseInteger_7.java
@@ -0,0 +1,27 @@
+package com.jk.solution;
+
+public class ReverseInteger_7 {
+
+    public static int reverse(int x) {
+        int left = x;
+        int ret = 0;
+        while (left != 0) {
+            if (Math.abs(ret) > Integer.MAX_VALUE / 10) { //overflow
+                return 0;
+            }
+            ret *= 10;
+
+            int remainder = left % 10;
+
+            if (ret > 0 && ret > Integer.MAX_VALUE - remainder
+                    || ret < 0 && ret < Integer.MIN_VALUE - remainder) { //overflow
+                return 0;
+            }
+            ret += remainder;
+
+            left = left / 10;
+        }
+
+        return ret;
+    }
+}
