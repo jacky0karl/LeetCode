314f3a83305836bdb91bca0f873b79b56bfdb029
jacky0karl <jacky0karl@gmail.com>
2017-09-25 18:56:42 +0800

    modify solution 3

--- src/com/jk/solution/LongestSubstring_3.java
+++ src/com/jk/solution/LongestSubstring_3.java
@@ -1,37 +1,53 @@
 package com.jk.solution;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class LongestSubstring_3 {
 
     public static void test() {
         System.out.println("2 Longest Substring:");
-        int ret = lengthOfLongestSubstring("sbdvdf");
-       // int ret = lengthOfLongestSubstring("xgpdwwklew");
-      //   int ret = lengthOfLongestSubstring("abcabcbb");
+        int ret = lengthOfLongestSubstring("abba");
+        //   int ret = lengthOfLongestSubstring("pwwkew");
+        //  int ret = lengthOfLongestSubstring("abcabcbb");
         System.out.println(String.valueOf(ret));
     }
 
+    // O(n)
     public static int lengthOfLongestSubstring(String s) {
+        int ret = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0, i = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            if (map.containsKey(c)) {
+                i = Math.max(map.get(c) + 1, i);
+            }
+            ret = Math.max(ret, j - i + 1);
+            map.put(c, j);
+        }
+        return ret;
+    }
+
+    // O(2n)
+    public static int lengthOfLongestSubstring1(String s) {
         if (s == null) {
             return 0;
         }
 
         int n = s.length();
-        HashMap<Character, Integer> map = new HashMap<>();
-        int ret = 0, left = 0, right = 0;
-        while (left < n && right < n) {
-            char c = s.charAt(right);
-            if (map.containsKey(c)) {
-                map.remove(s.charAt(left));
-                left++;
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
             } else {
-                map.put(c, right);
-                right++;
-                ret = Math.max(ret, right - left);
+                set.remove(s.charAt(i++));
             }
         }
-        return ret;
+        return ans;
     }
 
     public static int lengthOfLongestSubstring2(String s) {
